05bb4258cb94c55e44ebe13d1d20c2d73e730c48
Maxim Mazhuga <everly@bk.ru>
2017-03-18 14:59:06 +0300

    Initial Commit

---
+++ GPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPS
+//
+//  Created by Maxim Mazhuga on 18.03.17.
+//  Copyright © 2017 Maxim Mazhuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
